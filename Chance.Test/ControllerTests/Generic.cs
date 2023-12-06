527c3e845891921c3dde1d58011f1a8e46c61a0b
siljavind <silja.vj@gmail.com>
2023-12-06 14:22:20 +0100

    test error

---
+++ Chance.Test/ControllerTests/Generic.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chance.Test.ControllerTests
+{
+    public class Generic
+    {
+        [Fact]
+        public async Task GetAll_ReturnsOkResult_WithListOfEntities()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Chance.API;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
